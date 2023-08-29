26683723f43c855e66f426c40fee785dd71e21f3
Dev-JeanCharles <jeancduasan@gmail.com>
2023-08-29 19:42:01 -0300

    Started Project

---
+++ src/main/java/com/academy/fourtk/treinamentoapirest/TreinamentoApirestApplication.java
@@ -0,0 +1,13 @@
+package com.academy.fourtk.treinamentoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/fourtk/treinamentoapirest/TreinamentoApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.fourtk.treinamentoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
