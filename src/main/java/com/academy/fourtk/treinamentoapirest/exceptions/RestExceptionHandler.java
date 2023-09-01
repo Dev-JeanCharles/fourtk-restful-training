1b8900215fc66de8dc2f590f1dd397613a3a0709
Dev-JeanCharles <jeancduasan@gmail.com>
2023-09-01 18:26:19 -0300

    Implemented Handling Bad Request Exception

--- src/main/java/com/academy/fourtk/treinamentoapirest/exceptions/RestExceptionHandler.java
+++ src/main/java/com/academy/fourtk/treinamentoapirest/exceptions/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.academy.fourtk.treinamentoapirest.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,8 +28,38 @@ public class RestExceptionHandler {
         return ResponseEntity.status(status).body(error);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardError> httpMessageNotReadableException(HttpMessageNotReadableException br, HttpServletRequest h) {
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError error = new StandardError();
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Bad Request");
+        error.setPath(h.getRequestURI());
+        error.setMessage(br.getMessage());
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<StandardError> badRequest(BadRequestException br, HttpServletRequest h) {
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError error = new StandardError();
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Bad Request");
+        error.setPath(h.getRequestURI());
+        error.setMessage(br.getMessage());
+
+        return ResponseEntity.status(status).body(error);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationError> validationBadlyFormatted(MethodArgumentNotValidException r, HttpServletRequest h) {
+    public ResponseEntity<ValidationError> validationBadlyFormatted(MethodArgumentNotValidException v, HttpServletRequest h) {
 
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
         ValidationError error = new ValidationError();
@@ -37,9 +68,9 @@ public class RestExceptionHandler {
         error.setStatus(status.value());
         error.setError("Validation Exception");
         error.setPath(h.getRequestURI());
-        error.setMessage(r.getMessage());
+        error.setMessage(v.getMessage());
 
-        for (FieldError f : r.getBindingResult().getFieldErrors()) {
+        for (FieldError f : v.getBindingResult().getFieldErrors()) {
             error.addError(f.getField(),f.getDefaultMessage());
         }
 
